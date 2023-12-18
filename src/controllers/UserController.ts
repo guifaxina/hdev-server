7379bdb7b02f1db015ef4e26b04ab12c43a4b962
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-12-18 20:53:55 -0300

    chore: create createUser controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import UserService from "../services/UserService.js"
+import UserRepository from "../repositories/UserRepository.js"
 import { Request, Response } from "express"
 
 class UserController {
@@ -10,10 +11,17 @@ class UserController {
 
   public createS3UploadUrl = async (_: Request, res: Response): Promise<void> => {
     const signedUrl = await this.userService.createS3UploadUrl()
-    res.json({ signedUrl })
+    res.status(200).json({ status: "success", signedUrl, message: "pre-signed url created successfully." })
+  }
+
+  public createUser = async (_: Request, res: Response): Promise<void> => {
+    const userCreated = await this.userService.createUser()
+    res.status(200).json({ status: "success", userCreated, message: "user created successfully."})
   }
 }
 
-const service = new UserService()
+const repository = new UserRepository()
+
+const service = new UserService(repository)
 
 export default new UserController(service)
