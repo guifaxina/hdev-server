6bf5a1a8e3b6dc547c0f6694cc93d26e6dac42a3
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-12-19 02:31:57 -0300

    feat: create healthcheck endpoint

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,9 +1,13 @@
-import Router from "express";
+import Router, { Request, Response } from "express";
 import UserController from "../controllers/UserController.js";
 
 const router = Router();
 
 router.get("/get-upload-profilepic-url", UserController.createS3UploadUrl)
+
+router.get("/healthcheck", (_: Request, res: Response) => {
+  res.status(200)
+})
 
 router.post("/create-user", UserController.createUser)
 
