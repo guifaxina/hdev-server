abb86fbb70d5a1348032f4dcacb31237f5adf40d
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-12-18 23:54:52 -0300

    fix: remove testing parameters

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,12 +1,10 @@
-import Router, { Request, Response } from "express";
+import Router from "express";
 import UserController from "../controllers/UserController.js";
 
 const router = Router();
 
 router.get("/get-upload-profilepic-url", UserController.createS3UploadUrl)
 
-router.get("/oi", (_: Request, res: Response) => {
-  res.json({ msg: "oi" })
-})
+router.post("/create-user", UserController.createUser)
 
 export default router
